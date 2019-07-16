4f83f6940101a938e4cea9cbc53f73747c7796f6
Gustavo Morais <gustavo.morais@cabify.com>
2019-07-16 15:14:53 -0300

    Go By Example

--- Untitled-1.go
+++ Untitled-1.go
@@ -1,11 +1,32 @@
 package main
-
 import "fmt"
+func main() {
 
-func main()  { 
-	var nome string
+  var n1 float64 = 20
+  var n2 float64 = 10
+  const R1 int = 10
+     fmt.Println( "Qual o valor da multiplicaçao dos valores", n1, "e", n2, "?")
+    
+    if n1 / n2 == 10{
+        fmt.Println (n1*n2)
+          fmt.Println ( "esta correto!! A resposta é", R1 ) 
 
-	nome = "Gustavo"
-	
-	fmt.Print(nome)	
+  } else if n1*n2 != 10 {
+    fmt.Println( "Esta incsorreto!! \n o valor correto seria", R1 )
+  }
+   i := 1 
+    for i <= R1 {
+      fmt.Println(i)
+      i = i + 1
+    }
+  for {
+      fmt.Print("loop")
+      break
+      } 
+      for i1 := -1; i1 <= R1 ; i1++ {
+        if i1 % 2 == 0 {
+          continue
+    }
+    fmt.Println(i1)
+  }
 }
